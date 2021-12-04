21b4c322e29f14fcd6a5c80d55ae4764ce9bd078
Ariane Brandão <ariane_mad@hotmail.com>
2021-12-04 14:43:39 -0300

    token and refresh token done

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
+import { signOut } from "../contexts/AuthContext";
 
 type FailedRequestQueue = {
   onSuccess: (token: string) => void;
@@ -87,7 +88,10 @@ api.interceptors.response.use(
         });
       } else {
         //deslogar usuario
+        signOut();
       }
     }
+
+    return Promise.reject(error);
   }
 );
